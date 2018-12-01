3c57fe78247d58bedb97bc2b1a49de4a72a18a79
hunnain <hunnainpashahgchgc@gmail.com>
2018-12-02 00:11:21 +0500

    Routers

---
+++ Routers/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Container, Text } from 'native-base';
+import { Router, Scene, Stack } from 'react-native-router-flux';
+import SplashScreen from '../Component/SplashScreen/index';
+
+class Routers extends Component {
+    render() {
+        return (
+            <Container>
+                <Router>
+                    <Stack key="root">
+                        <Scene key="home" hideNavBar={true} initial component={SplashScreen} />
+                    </Stack>
+                </Router>
+            </Container>
+        )
+    }
+}
+
+export default Routers;
