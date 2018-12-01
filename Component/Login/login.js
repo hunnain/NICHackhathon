81d9a7072884050e5825fcd3e72a0bb6144398dd
hunnain <hunnainpashahgchgc@gmail.com>
2018-12-02 00:12:15 +0500

    Login

---
+++ Component/Login/login.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {StyleSheet, Animated, View} from 'react-native';
+import { Container, Text, Spinner, Button} from 'native-base';
+
+
+class Login extends Component{
+    constructor(props){
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <Container>
+                
+            </Container>
+        )
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#0091E0',
+    },
+    welcome: {
+      fontSize: 40,
+      textAlign: 'center',
+      margin: 10,
+      color: '#E8EEF0'
+    },
+  });
+  
