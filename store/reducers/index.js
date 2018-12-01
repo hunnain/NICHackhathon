a36b1046a10580654d1aff2dceba5faaf9293c92
hunnain <hunnainpashahgchgc@gmail.com>
2018-12-02 00:11:38 +0500

    Reducers

---
+++ store/reducers/index.js
@@ -0,0 +1,7 @@
+import login_signup_reducer from './login_signup_reducers';
+import {combineReducers} from 'redux';
+
+var root = combineReducers({
+    login_signup_reducer,
+})
+export default root;
