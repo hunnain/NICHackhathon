c28525fe38fb26033ac4344a94277800f0e3f84a
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2018-12-23 16:09:38 +0500

    Updated
    Updated

--- Routers/index.js
+++ Routers/index.js
@@ -4,6 +4,7 @@ import { Router, Scene, Stack } from 'react-native-router-flux';
 import SplashScreen from '../Component/SplashScreen/index';
 import Login from '../Component/Login/login';
 
+// Routers
 class Routers extends Component {
     render() {
         return (
